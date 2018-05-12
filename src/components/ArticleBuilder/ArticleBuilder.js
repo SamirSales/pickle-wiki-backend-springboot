ebaa6b8b097278bd2b43a93c64e869c5fb78807e
SamirSales <samir.sribeiro@gmail.com>
2018-05-11 22:18:23 -0300

    setting ArticleBuilder with CKEditor

--- src/components/ArticleBuilder/ArticleBuilder.js
+++ src/components/ArticleBuilder/ArticleBuilder.js
@@ -1,35 +1,67 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
-import TemplateButton from '../../components/TemplateButton/TemplateButton';
 import './ArticleBuilder.css';
 
-import TextEditor from '../../components/TextEditor/TextEditor';
+import CKEditor from "react-ckeditor-component";
 
-const articleBuilder = ( props ) => {
+class ArticleBuilder extends Component {
 
-    return (
-        <Aux>
-            <h1 className='simple-template-title'><i className="fa fa-edit"></i> Novo artigo</h1>
-            <p className='article-builder-p'><b>Título <span className='needed'>*</span></b></p>
-            <input className="form-input" placeholder='Insira um título...' />
+    constructor(props) {
+        super(props);
+        this.updateContent = this.updateContent.bind(this);
 
-            <p className='article-builder-p'><b>Palavras chaves</b></p>
-            <input className="form-input" placeholder='Insira palavras chaves...' />
+        this.state = {
+            resumeContent: 'content',
+        }
+    }
+ 
+    updateContent(newContent) {
+        this.setState({
+            resumeContent: newContent
+        })
+    }
+    
+    onResumeContentChange = (evt) => {
+      var newContent = evt.editor.getData();
+      this.setState({
+        resumeContent: newContent
+      })
 
-            <p className='article-builder-p'><b>Resumo <span className='needed'>*</span></b></p>
-            {/* <textarea className="form-input" placeholder='Insira um resumo...' rows="5" /> */}
-            <TextEditor />
+      console.log(newContent);
+    }
 
-            <h2 className='article-builder-h2'>Tópicos & Templates</h2>
-            {/* <TemplateButton title='Texto' type='text'/>
-            <TemplateButton title='Imagem' type='image'/>
-            <TemplateButton title='Texto + Imagem' type='text-image'/> */}
+    render() {
+        return (
+            <Aux>
+                <h1 className='simple-template-title'><i className="fa fa-edit"></i> Novo artigo</h1>
+                <p className='article-builder-p'><b>Título <span className='needed'>*</span></b></p>
+                <input className="form-input" placeholder='Insira um título...' />
+    
+                <p className='article-builder-p'><b>Palavras chaves</b></p>
+                <input className="form-input" placeholder='Insira palavras chaves...' />
+    
+                <p className='article-builder-p'><b>Resumo <span className='needed'>*</span></b></p>
+                <CKEditor 
+                    activeClass="p10" 
+                    content={this.state.resumeContent} 
+                    events={{"change": this.onResumeContentChange }} />
+    
+                <h2 className='article-builder-h2'>Tópicos & Templates</h2>
+    
+                <button>Novo Tópico</button>
+    
+                <button>Submeter</button>
+                <button>Descartar</button>
+    
+                <br/><br/><br/><br/>
+            </Aux>
+        )
+    }
 
-            <button>Novo Tópico</button>
+    
+}
 
-        </Aux>
-    )
-};
-export default articleBuilder;
+
+export default ArticleBuilder;
