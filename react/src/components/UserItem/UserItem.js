dd45cb5162228f552a8e4ea7268050ced839d54a
SamirSales <samir.sribeiro@gmail.com>
2018-06-08 23:04:20 -0300

    UserItem component

---
+++ react/src/components/UserItem/UserItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './UserItem.css';
+import logo from '../../assets/img/profile.png';
+
+const userItem = ( props ) => {
+
+    return (
+        <div className="userItem">
+            
+            <div className="user-item-div-image">
+                <img src={logo} alt="Pickle Wiki" height="47" width="47"/>
+            </div>
+
+            <div className="user-item-div-info">
+                <h3 className="user-item-name">{props.name}</h3>
+                <h3 style={{width : '30%'}}>login: <span className="user-item-span">{props.login}</span></h3>
+                <h3 style={{width : '30%'}}>e-mail: <span className="user-item-span">{props.email}</span></h3>
+
+                <div className="user-item-div-options">
+                    <a className="fa fa-trash"></a>
+                    <a className="fa fa-edit"></a>                    
+                </div>
+            </div>
+            
+            
+            
+        </div>       
+    )
+};
+export default userItem;
--- react/src/containers/UserEditor/UserEditor.js
+++ react/src/containers/UserEditor/UserEditor.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Aux from '../../hoc/Aux';
+import UserItem from '../../components/UserItem/UserItem';
 import './UserEditor.css';
  
 class UserEditor extends Component {
 
+    state = {
+      users: []
+    }
+
     componentDidMount(){
       const users = axios.get('http://localhost:8080/users/')
         .then(response => {
-          console.log(response);
+          // console.log(response);
+          this.setState({users: response.data});
         });
     }
 
     render() {
     
+      const users = this.state.users.map(user => {
+        return <UserItem name={user.name} login={user.login} email={user.email}/>;
+      });
+
+      const emptyFieldMessage = <p className='empty-content-message'>Nenhum editor foi adicionado.</p>;
+
         return (
           <Aux>
               <h1 className='simple-template-title'><i className="fa fa-users"></i> Editores</h1>
               <div className='article-builder-div-content user-editor-div-user-list' style={{marginTop: '10px'}}>
-                <p className='empty-content-message'>Nenhum editor foi adicionado.</p>
+                {users}
+                {this.state.users.length === 0 ? emptyFieldMessage : null}
               </div>
 
               <button className='article-btn article-btn-topic' style={{marginTop: '10px'}}>Novo Editor</button>
