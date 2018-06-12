7d99c1ddab8fef9e83d2b60e6900159d5ed0f59a
SamirSales <samir.sribeiro@gmail.com>
2018-06-11 22:03:31 -0300

    axios settings

--- react/src/index.js
+++ react/src/index.js
@@ -5,5 +5,18 @@ import '../node_modules/font-awesome/css/font-awesome.min.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'http://localhost:8080';
+// axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+// axios.interceptors.request.use(request => {
+//     //Edit request config
+//     return request;
+// }, error =>{
+//     return Promise.reject(error);
+// })
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
