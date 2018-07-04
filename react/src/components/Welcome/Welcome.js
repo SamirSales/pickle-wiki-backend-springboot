10c8ead31c16b39c34fdd78088aed6b5f53cefa1
SamirSales <samir.sribeiro@gmail.com>
2018-07-04 20:39:00 -0300

    setting welcome page

--- react/src/components/Welcome/Welcome.js
+++ react/src/components/Welcome/Welcome.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
-import SimpleTemplate from '../../components/SimpleTemplate/SimpleTemplate';
+import ReactMarkdown from 'react-markdown';
 
 const welcome = ( props ) => {
 
+    const body = "# Bem vindo ao Pickle Wiki \r"+
+    "A Pickle Wiki é um projeto de enciclopédia de licença livre, baseado no sistema "+
+    "[Wikipédia](https://www.wikipedia.org/) e escrito de maneira colaborativa. "+
+    "Seu código fonte encontra-se no github, atualmente, sob administração de seu criador, Samir Sales. "+
+    "O Pickle Wiki é uma ferramenta de pesquisa com foco em redes internas.\r"+
+    
+    "## De onde veio esse nome?\r"+
+    "O nome Pickle Wiki é uma paródia do nome *Wikipédia* combinada com a expressão *“Pickle Rick”* "+
+    "da animação americana Rick and Morty, criada por *Justin Roiland* e *Dan Harmon*.";
+
     return (
-        <SimpleTemplate title="Bem vindo ao Pickle Wiki"
-            text="O Pickle Wiki é uma ferramente que..." />
+        <div className='text-editor-markdown'><ReactMarkdown source={body} /></div>
     )
 };
 export default welcome;
