2ae3689a71bb631ec88b33b6218a74f67d26f88b
SamirSales <samir.sribeiro@gmail.com>
2018-07-16 22:49:09 -0300

    implementing authorization

--- springBoot/src/main/java/io/github/samirsales/Security/SecurityConfig.java
+++ springBoot/src/main/java/io/github/samirsales/Security/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
