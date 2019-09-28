248909faf1847b7658dee8939b9bc6038faf4e64
SamirSales <samir.sribeiro@gmail.com>
2019-09-28 14:35:32 -0300

    refactoring

--- react/src/config.js
+++
@@ -1,9 +0,0 @@
-
-/* It avoids the mess of IP change from the server. */
-export const TAG_OF_ARTICLE_IMAGE = "<pickle-wiki-article-image-tag>";
-
-export const URL_IMAGES = "http://localhost:8080/pickle-wiki-image";
-
-export const URL_IMAGES_PROFILE = "http://localhost:8080/pickle-wiki-image/profiles";
-
-export const URL_HOME_PAGE = "/pickle_wiki";
