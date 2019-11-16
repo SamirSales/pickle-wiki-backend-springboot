a3873ec4e794b338c7694a4c151c979b0271bbfb
SamirSales <samir.sribeiro@gmail.com>
2019-11-16 15:47:44 -0300

    updating UserControllerRestTest.java

--- src/test/java/io/github/samirsales/controller/UserControllerRestTest.java
+++ src/test/java/io/github/samirsales/controller/UserControllerRestTest.java
@@ -1,5 +1,6 @@
 package io.github.samirsales.controller;
 
+import io.github.samirsales.model.dto.CredentialDTO;
 import io.github.samirsales.repository.UserRepository;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,10 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -26,22 +24,41 @@ public class UserControllerRestTest {
     private UserRepository userRepository;
 
     @Test
-    public void verifyIfGetAllIsForbidden(){
+    public void verifyIfGetAllIsForbidden() {
         final HttpEntity<?> requestEntity = null;
         ResponseEntity<String> responseEntity = testRestTemplate
                 .exchange("/users", HttpMethod.GET, requestEntity, String.class);
         Assert.assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
     }
 
-//    @Test
-//    public void verifyIfGetAllIsOk() throws Exception {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Authorization", "its_value");
-//
-//        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
-//
-//        ResponseEntity<String> responseEntity = testRestTemplate
-//                .exchange("/users", HttpMethod.GET, requestEntity, String.class);
-//        Assert.assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
-//    }
+    @Test
+    public void verifyIfGetAllIsOk() {
+        String authenticationToken = getAuthenticationToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", authenticationToken);
+
+        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> responseEntity = testRestTemplate
+                .exchange("/users", HttpMethod.GET, requestEntity, String.class);
+        Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    private String getAuthenticationToken() {
+        ResponseEntity<String> responseEntityAuthentication = getResponseEntityAuthentication();
+        return responseEntityAuthentication.getHeaders().getFirst("Authorization");
+    }
+
+    private ResponseEntity<String> getResponseEntityAuthentication() {
+        CredentialDTO credentialDTO = getAdminCredentialDTO();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<CredentialDTO> requestEntity = new HttpEntity<>(credentialDTO, headers);
+        return testRestTemplate.exchange("/login", HttpMethod.POST, requestEntity, String.class);
+    }
+
+    private CredentialDTO getAdminCredentialDTO(){
+        String username = "admin";
+        String password = "admin";
+        return new CredentialDTO(username, password);
+    }
 }
